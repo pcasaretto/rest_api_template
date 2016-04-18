d4becb963d844ab2a646917492be0090a4e9e3f5
Hugo Luchessi <hugo.luchessi@resultadosdigitais.com.br>
2016-04-18 17:37:40 -0300

    Removed Test Api remnants

--- lib/test_api/controllers/health_check_controller.rb
+++ lib/test_api/controllers/health_check_controller.rb
@@ -5,8 +5,6 @@ module TestApi
   module Controllers
     class HealthCheckController < TestApi::Main
       get '/' do
-        sql = "( (select id from leads_2 lead inner join k_email em on em.entity_id = lead.id and em.value = 'lead@lead.com.br') union all (select id from leads_2 lead inner join k_opportunity ko on ko.entity_id = lead.id and ko.value = 'not_opportunity') union all (select id from leads_2 lead inner join ( select relation.entity_id from r_pgv_date relation where relation.value >= '2015-07-30' ) tbl_aggr_1 on tbl_aggr_1.entity_id = lead.id ) union all (select id from leads_2 lead inner join ( select relation.entity_id from r_cnv_identifier relation where relation.value = 'webinar-gestao_leads' ) tbl_aggr_1 on tbl_aggr_1.entity_id = lead.id ) union all (select id from leads_2 lead inner join k_job_title ko on ko.entity_id = lead.id and to_tsquery('portuguese', 'diretor') @@ to_tsvector('portuguese', ko.value) ) ) limit 20"
-        ActiveRecord::Base.connection.execute(sql)
       end
 
       get '/status' do
