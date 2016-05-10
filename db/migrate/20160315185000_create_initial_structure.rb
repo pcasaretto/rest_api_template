68a5d3468e4846d9e7c7db682127a83a540454de
Hugo Luchessi <hugo.luchessi@resultadosdigitais.com.br>
2016-05-10 18:27:22 -0300

    More template corrections

--- db/migrate/20160315185000_create_initial_structure.rb
+++ db/migrate/20160315185000_create_initial_structure.rb
@@ -1,17 +1,17 @@
 class CreateInitialStructure < ActiveRecord::Migration
   def change
-    create_table :model1 do |t|
+    create_table :model1s do |t|
       t.string :name, limit: 60, null: false
       t.column :id_model2, :integer, null: false
     end
 
-    create_table :model2 do |t|
+    create_table :model2s do |t|
       t.string :name, limit: 60, null: false
     end
 
-    add_foreign_key :model1, :model2, column: :id_model2
+    add_foreign_key :model1s, :model2s, column: :id_model2
 
-    add_index(:model2, :name)
-    add_index(:model1, [:id_model2, :name])
+    add_index(:model2s, :name)
+    add_index(:model1s, [:id_model2, :name])
   end
 end
