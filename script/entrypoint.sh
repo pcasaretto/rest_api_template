a87eb0e310837713b152a45c94a59e3c66b027e5
hugoluchessi <hugoluchessi@gmail.com>
2016-09-04 19:31:27 -0300

    More bug fixes

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,37 +1,42 @@
 #!/bin/bash
+
 rm -f /var/app/tmp/pids/server.pid
 
-header_indicator="==========>"
+header_indicator=""
 command_indicator=">"
 pg_connection_attempt=1
 max_pg_attempts=20
 
+echo_command(){
+  echo "=> $1"
+}
+
 wait_for_db(){
-  echo "$header_indicator Connecting to $1 container"
+  echo "==========> Connecting to $1 container"
   until psql -h $1 -U "postgres" -c '\l' > /dev/null 2>&1; do
-    echo "$command_indicator Waiting for $1 container...(${pg_connection_attempt}/${max_pg_attempts})"
+    echo_command "Waiting for $1 container...(${pg_connection_attempt}/${max_pg_attempts})"
     sleep 1
     if [[ $pg_connection_attempt -gt $max_pg_attempts-1 ]]; then
-      echo "$header_indicator Failure: The web process could not connect to $1 container after $pg_connection_attempt attempts"
-      echo "$command_indicator Please try again running ./script/rd-docker start."
+      echo_command "Failure: The web process could not connect to $1 container after $pg_connection_attempt attempts"
+      echo_command "Please try again running ./script/rd-docker start."
       exit 1;
     fi
     pg_connection_attempt=$((pg_connection_attempt+1))
   done
-  echo "$command_indicator $1 container up and running!"
+  echo_command "$1 container up and running!"
 }
 
 ensure_db(){
-  echo "$command_indicator Checking Development $1"
+  echo_command "Checking Development $1"
   has_db=$(psql -h $1 -U postgres -lqt | cut -d \| -f 1 | grep -w $2 | wc -l )
 
   if [[ "$has_db" == 0 ]]; then
-    echo "$command_indicator $1 setup is necessary"
-    bundle exec rake db:setup
+    echo_command "$1 setup is necessary"
+    bundle exec rake create_db
   else
-    echo "$command_indicator $1 already exists. Skiping database setup."
-    echo "$command_indicator Checking for pending migrations."
-    bundle exec rake db:migrate
+    echo_command "$1 already exists. Skiping database setup."
+    echo_command "Checking for pending migrations."
+    bundle exec rake migrate_db
   fi
 }
 
